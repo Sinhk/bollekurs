bb7b58fb38815adac2f062247a6bdad5a9d6c4b2
Sindre Kroknes <sindre.kroknes@gmail.com>
2019-01-30 23:43:10 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tur.Models;
+
+namespace Tur.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly string[] text = new []{"kanotur","sykkeltur","boring","byhaik"};
+        private readonly Random random = new Random();
+        private static int counter = 0;
+
+        public IActionResult Index()
+        {
+            Console.WriteLine(HttpContext.Connection.RemoteIpAddress);
+            //Console.WriteLine(HttpContext.Connection.Remote);
+            return View(text[counter++%4]);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
