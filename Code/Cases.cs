e47328baf38fe741e9f73a14c4477c387e972758
sindre <sindre.kroknes@gmail.com>
2023-02-04 13:16:56 +0100

    disabled some hike cases

--- Code/Cases.cs
+++ Code/Cases.cs
@@ -20,14 +20,14 @@ static class Cases
 
     public static readonly IDictionary<string, Case> Haiker = new Dictionary<string, Case>
         {
-            {"sykkel", new Case("Sykkel haik","Du skal argumentere for at haiken skal være med sykkel",new[]{"haik/sykkeltur.jpg"})},
+            //{"sykkel", new Case("Sykkel haik","Du skal argumentere for at haiken skal være med sykkel",new[]{"haik/sykkeltur.jpg"})},
             {"kano", new Case("Kano haik","Du skal argumentere for at haiken skal være med kano",new[]{"haik/kanotur.jpg","haik/kanotur2.jpg"})},
             {"ikke", new Case("Ikke haik","Du skal argumentere for å ikke ha haik",new[]{"haik/ikketur.jpg"})},
             {"by", new Case("By haik","Du skal argumentere for en by haik",new[]{"haik/byhaik.jpg"})},
             {"lang", new Case("Lang haik","Du skal argumentere for en lang haik",new []{"haik/Langtur haik.JPG"})},
-            {"kort", new Case("Kort haik","Du skal argumentere for en kort haik",new []{"haik/kort haik.JPG"})},
-            {"mat", new Case("Mat haik","Du skal argumentere for en mat-haik. \n En haik med god mat",new []{"haik/Mathaik.JPG"})},
+            //{"kort", new Case("Kort haik","Du skal argumentere for en kort haik",new []{"haik/kort haik.JPG"})},
+            //{"mat", new Case("Mat haik","Du skal argumentere for en mat-haik. \n En haik med god mat",new []{"haik/Mathaik.JPG"})},
             {"noysomhet", new Case("Nøysomhets haik","Du skal argumentere for en nøysomhets haik. \n Matbudsjett på 26kr per person.",new []{"haik/noysomhet.jpg"})},
-            {"jakke", new Case("Jakke haik","Du skal argumentere for en jakke haik. \n Du får kun ha med det du får plass til i ei jakke",new []{"haik/jakke.jpg"})},
+            //{"jakke", new Case("Jakke haik","Du skal argumentere for en jakke haik. \n Du får kun ha med det du får plass til i ei jakke",new []{"haik/jakke.jpg"})},
         };
 }
